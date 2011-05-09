e6d3aa635fe4197cce3e5c3ac72b305666934b64
mhl.bocian@gmail.com <mhl.bocian@gmail.com@7e1661bb-de51-e99c-37c1-f5beeeb525d3>
2011-05-09 17:00:29 +0000

    Zmiany w WebServices

--- branches/planlekcji-1.0dev/services/webapi.php
+++ branches/planlekcji-1.0dev/services/webapi.php
@@ -12,6 +12,21 @@
 require_once '../lib/nusoap/nusoap.php';
 require_once '../modules/isf/classes/kohana/isf.php';
 
+function is_installed() {
+    $isf = new Kohana_Isf();
+    $isf->DbConnect();
+    if (!file_exists('../modules/isf/isf_resources/default.sqlite')) {
+        echo 'Uslugi WebAPI sa niedostepne dopoki system nie zostanie zainstalowany';
+        exit;
+    } else {
+        $ctb = $isf->DbSelect('sqlite_master', array('*'), 'where name="rejestr"');
+        if (count($ctb) == 0) {
+            echo 'Uslugi WebAPI sa niedostepne dopoki system nie zostanie zainstalowany';
+            exit;
+        }
+    }
+}
+
 $ns = 'com.intersys.planlekcji';
 $srv = new soap_server();
 $srv->configureWSDL('sysinfo', $ns);
@@ -19,32 +34,13 @@ $srv->configureWSDL('sysinfo', $ns);
 
 $srv->register('is_installed', array(), array('return' => 'xsd:boolean'), $ns);
 
-function is_installed() {
-    $isf = new Kohana_Isf();
-    $isf->DbConnect();
-    if (!file_exists('../modules/isf/isf_resources/default.sqlite')) {
-        return false;
-    } else {
-        $ctb = $isf->DbSelect('sqlite_master', array('*'), 'where name="rejestr"');
-        if (count($ctb) != 0) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-}
-
 $srv->register('version', array(), array('return' => 'xsd:string'), $ns);
 
 function version() {
     $isf = new Kohana_Isf();
     $isf->DbConnect();
-    if (!is_installed()) {
-		return '';
-    } else {
-        $res = $isf->DbSelect('rejestr', array('*'), 'where opcja="app_ver"');
-        return $res[1]['wartosc'];
-    }
+    $res = $isf->DbSelect('rejestr', array('*'), 'where opcja="app_ver"');
+    return $res[1]['wartosc'];
 }
 
 $srv->service($HTTP_RAW_POST_DATA);
