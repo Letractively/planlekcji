d59f08191f82e26f5184da128926444a41494c44
mhl.bocian@gmail.com <mhl.bocian@gmail.com@7e1661bb-de51-e99c-37c1-f5beeeb525d3>
2011-06-06 15:06:49 +0000

    UTWORZENIE GAŁĘZI DLA POSTGRESQL ZAMIAST SQLITE3

---
+++ branches/planlekcji-1.0dev-pgsql/application/classes/controller/podglad.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * Intersys - Plan Lekcji
+ * 
+ * Wersja pierwsza - 1.0
+ * 
+ * @author Michał Bocian <mhl.bocian@gmail.com>
+ */
+defined('SYSPATH') or die('No direct script access.');
+/**
+ * Kontroler: podglad
+ * 
+ * Rola: Odpowiada za wyswietlanie danych
+ */
+class Controller_Podglad extends Controller {
+    
+    public function __construct() {
+        session_start();
+    }
+    
+    public function action_klasa($klasa){
+        $view = view::factory('podglad_klasa');
+        $view->set('klasa', $klasa);
+        echo $view->render();
+    }
+    
+    public function action_sala($klasa){
+        $view = view::factory('podglad_sala');
+        $view->set('klasa', $klasa);
+        echo $view->render();
+    }
+    
+    public function action_nauczyciel($klasa)
+    {
+        $view = view::factory('podglad_nauczyciel');
+        
+        $isf = new Kohana_Isf();
+        $isf->DbConnect();
+        
+        $imienaz = $isf->DbSelect('nauczyciele', array('*'), 'where skrot="'.$klasa.'"');
+        $imienaz = $imienaz[1]['imie_naz'];
+        
+        $view->set('skrot', $klasa);
+        $view->set('klasa', $imienaz);
+        echo $view->render();
+    }
+    
+}
