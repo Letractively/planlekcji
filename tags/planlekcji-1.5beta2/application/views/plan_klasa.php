407b4408f1907a123b9165d109e9ecebde01fd97
mhl.bocian@gmail.com <mhl.bocian@gmail.com@7e1661bb-de51-e99c-37c1-f5beeeb525d3>
2011-06-09 18:24:18 +0000

    WYDANIE BETA2 =============  - Nowy instalator poprzez przeglądarkę WWW  - automatyczne dopisanie zmiennej $path (config.php)  - automatyczne chmody (index.php)  - Poprawki w szablonach edycji planu  - Błąd z nieprawidłowym wyświetlaniem w IE, następuje przekierowanie do innej strony bez elementu div:absolute  - Dodanie favicon.ico  - Poprawki w Kohana_Isf (funkcja detect_ie)

---
+++ tags/planlekcji-1.5beta2/application/views/plan_klasa.php
@@ -0,0 +1,36 @@
+<?php
+$k = $klasa;
+$isf = new Kohana_Isf();
+$isf->DbConnect();
+$reg = $isf->DbSelect('rejestr', array('*'), 'where opcja=\'edycja_danych\'');
+$ns = $isf->DbSelect('rejestr', array('*'), 'where opcja=\'nazwa_szkoly\'');
+
+$isf->JQUi();
+$isf->JQUi_AjaxdivDoAjax('progress', URL::site('plan/klasaajax/' . $klasa), true);
+if ($isf->detect_ie()):
+    Kohana_Request::factory()->redirect('plan/klasaajax/'.$klasa.'/true');
+endif;
+?>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Plan lekcji - <?php echo $ns[1]['wartosc']; ?></title>
+        <link rel="stylesheet" type="text/css" href="<?php echo URL::base() ?>lib/css/style.css"/>
+    </head>
+    <body>
+        <div style="position: fixed; top: 0px; width: 100%; height: 80px; background: white;">
+            <h1>
+                <a href="#" onClick="document.forms['formPlan'].submit();">
+                    <img src="<?php echo URL::base() ?>lib/images/save.png" alt="zapisz"/></a>
+                Edycja planu dla <?php echo $klasa; ?>
+            </h1>
+            <p class="error">&bull; Zapisanie planu spowoduje usunięcie planu dla grup dla danej klasy</p>
+            <br/>
+        </div>
+        <div style="margin-top: 120px">
+            <?php echo $isf->JQUi_AjaxdivCreate('progress', true, false, '<b>Przeglądarka nie obsługuje JavaScript? Spróbuj <a href="' . URL::site('plan/klasaajax/' . $klasa . '/true') . '">metodę alternatywną</a></b>'); ?>
+        </div>
+        <?php echo $isf->JQUi_MakeScript(); ?>
+    </body>
+</html>
---
+++ tags/planlekcji-1.5beta2/install.php
@@ -0,0 +1,374 @@
+<?php
+/*
+ * Plik instalacyjny Planu Lekcji
+ */
+if (!file_exists('index.php')) {
+    die('Musisz uruchomic plik z poziomu katalogu aplikacji');
+}
+require_once 'modules/isf/classes/kohana/isf.php';
+$isf = new Kohana_Isf();
+$isf->DbConnect();
+$err = '';
+if (file_exists('modules/isf/isf_resources/default.sqlite') && !is_writable('modules/isf/isf_resources/default.sqlite')) {
+    $err .= 'Plik modules/isf/isf_resources/default.sqlite musi byc zapisywalny! Instalacja przerwana ';
+}
+if (!empty($err)) {
+    die($err);
+}
+$ctb = $isf->DbSelect('sqlite_master', array('*'), 'where name="rejestr"');
+if (count($ctb) != 0) {
+    $res = $isf->DbSelect('rejestr', array('*'), 'where opcja="installed"');
+    if (count($res) >= 1) {
+        $r = 1;
+    }
+} else {
+    $r = 0;
+}
+?>
+<?php if ($r == 1): ?>
+    <html>
+        <head>
+            <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+            <title>Instalacja Intersys Plan Lekcji</title>
+            <link rel="stylesheet" type="text/css" href="lib/css/style.css"/>
+        </head>
+        <body>
+            <img src="lib/images/logo.png"/>
+            <h1>Instalacja zakończona powodzeniem!</h1>
+            <h3>Usuń plik <b>install.php</b> i zaloguj się, używając
+                danych podanych przez instalator</h3>
+            <?php if ($fcfg == true): ?>
+                <?php
+                $r = $_SERVER['REQUEST_URI'];
+                $r = str_replace('index.php', '', $r);
+                $r = str_replace('install.php', '', $r);
+                ?>
+                <h3>Plik config.php nie istnieje! Proszę go utworzyć</h3>
+                <p>Treść pliku config.php</p>
+                <pre>
+                    <?php echo '<?php'; ?>
+                                $path = '<?php echo $r; ?>';
+                    <?php echo '?>'; ?>
+                </pre>
+            <?php endif; ?>
+        </body>
+    </html>
+    <?php exit; ?>
+<?php else: ?>
+    <?php if (!isset($_POST['step2'])): ?>
+        <html>
+            <head>
+                <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+                <title>Instalacja Intersys Plan Lekcji</title>
+                <link rel="stylesheet" type="text/css" href="lib/css/style.css"/>
+                <style type="text/css">
+                    pre{
+                        font-size: 10pt;
+                    }
+                </style>
+            </head>
+            <body>
+                <img src="lib/images/logo.png"/>
+                <h1>Instalator Intersys Plan Lekcji</h1>
+                <?php if ($_SERVER['SERVER_NAME'] != 'localhost' && $_SERVER['SERVER_NAME'] != '127.0.0.1'): ?>
+                    <p class="error">
+                        Aplikacja może zostać zainstalowana tylko wtedy, gdy ta
+                        strona jest wywołana z komputera lokalnego.
+                    </p>
+                <?php else: ?>
+                    <?php
+                    $r = $_SERVER['REQUEST_URI'];
+                    $r = str_replace('index.php', '', $r);
+                    $r = str_replace('install.php', '', $r);
+                    $r = str_replace('?err', '', $r);
+                    ?>
+                    <h3>Krok 1</h3>
+                    <form action="" method="post">
+                        <b>Nazwa szkoły: </b>
+                        <input type="text" name="inpSzkola" size="80"/><p/>
+                        <b>Ścieżka aplikacji*: </b>
+                        <input type="text" name="inpPath" size="50" value="<?php echo $r; ?>"/>
+                        <input type="hidden" name="step2" value="true"/><p/>
+                        <p class="info">
+                            Ścieżka aplikacji to ciąg znaków po nazwie hosta w pasku adresu
+                            przeglądarki. System automatycznie dopasuje odpowiednią wartość.
+                            Proszę nie zmieniać wartości tego pola chyba, że jest ona nieprawidłowa.
+                        </p>
+                        <button type="submit" name="btnSubmit">Zainstaluj aplikację</button>
+                    </form>
+                    <?php if (isset($_GET['err'])): ?>
+                        <p class="error">Żadne pole nie może być puste!</p>
+                    <?php endif; ?>
+                <?php endif; ?>
+            </body>
+        </html>
+    <?php else: ?>
+        <?php
+        if (empty($_POST['inpSzkola']) || $_POST['inpSzkola'] == ''):
+            header('Location: install.php?err');
+            exit;
+        endif;
+        echo '<link rel="stylesheet" type="text/css" href="lib/css/style.css"/>';
+        echo '<h1>Proces instalacji</h1><p class="info">Na dole strony znajduja sie dane
+            do logowania!</p><pre>';
+        $szkola = $_POST['inpSzkola'];
+        $a = fopen('config.php', 'w');
+        if (!$a) {
+            $ferr = true;
+        } else {
+            $file = '<?php' . PHP_EOL . '$path = \'' . $_POST['inpPath'] . '\';' . PHP_EOL . '?>';
+            fputs($a, $file);
+            fclose($a);
+        }
+        print <<< START
+
++ + + + + + + + + + + + + + + + + + + +
++                                     +
++    I   N   T   E    R   S   Y   S   +  Wersja 1.5
++    P  L  A  N   L  E  K  C  J  I    +  UNSTABLE
++                                     +
++ + + + + + + + + + + + + + + + + + + +
+
+
+Trwa instalacja systemu Intersys Plan Lekcji...
+
+START;
+
+        print <<< START
+Tworzenie tabeli: przedmioty
+
+START;
+
+        $isf->DbTblCreate('przedmioty', array(
+            'przedmiot' => 'text not null'
+        ));
+
+        print <<< START
+Tworzenie tabeli: sale
+
+START;
+
+        $isf->DbTblCreate('sale', array(
+            'sala' => 'text not null'
+        ));
+
+        print <<< START
+Tworzenie tabeli: przedmiot_sale
+
+START;
+
+        $isf->DbTblCreate('przedmiot_sale', array(
+            'przedmiot' => 'text not null',
+            'sala' => 'text not null'
+        ));
+
+        print <<< START
+Tworzenie tabeli: klasy
+
+START;
+
+        $isf->DbTblCreate('klasy', array(
+            'klasa' => 'text not null'
+        ));
+
+        print <<< START
+Tworzenie tabeli: nauczyciele
+
+START;
+
+        $isf->DbTblCreate('nauczyciele', array(
+            'imie_naz' => 'text not null',
+            'skrot' => 'text not null'
+        ));
+
+        print <<< START
+Tworzenie tabeli: nl_przedm
+
+START;
+
+        $isf->DbTblCreate('nl_przedm', array(
+            'nauczyciel' => 'text not null',
+            'przedmiot' => 'text not null'
+        ));
+
+        print <<< START
+Tworzenie tabeli: nl_klasy
+
+START;
+
+        $isf->DbTblCreate('nl_klasy', array(
+            'nauczyciel' => 'text not null',
+            'klasa' => 'text not null'
+        ));
+
+        print <<< START
+Tworzenie tabeli: rejestr
+
+START;
+
+        $isf->DbTblCreate('rejestr', array(
+            'opcja' => 'text not null',
+            'wartosc' => 'text'
+        ));
+
+        print <<< START
+Tworzenie tabeli: planlek
+
+START;
+
+        $isf->DbTblCreate('planlek', array(
+            'dzien' => 'text',
+            'klasa' => 'text',
+            'lekcja' => 'text',
+            'przedmiot' => 'text',
+            'sala' => 'text',
+            'nauczyciel' => 'text',
+            'skrot' => 'text'
+        ));
+
+        print <<< START
+Tworzenie tabeli: uzytkownicy
+
+START;
+
+        $isf->DbTblCreate('uzytkownicy', array(
+            'uid' => 'integer primary key autoincrement not null',
+            'login' => 'text not null',
+            'haslo' => 'text not null'
+        ));
+
+        print <<< START
+Tworzenie tabeli: plan_grupy
+
+START;
+
+        $isf->DbTblCreate('plan_grupy', array(
+            'dzien' => 'text',
+            'lekcja' => 'text',
+            'klasa' => 'text',
+            'grupa' => 'text',
+            'przedmiot' => 'text',
+            'nauczyciel' => 'text',
+            'skrot' => 'text',
+            'sala' => 'text'
+        ));
+
+        print <<< START
+Tworzenie tabeli: zast_id
+
+START;
+
+        $isf->DbTblCreate('zast_id', array(
+            'zast_id' => 'integer primary key autoincrement not null',
+            'dzien' => 'text',
+            'za_nl' => 'text',
+            'info' => 'text',
+        ));
+
+        print <<< START
+Tworzenie tabeli: zastepstwa
+
+START;
+
+        $isf->DbTblCreate('zastepstwa', array(
+            'zast_id' => 'text',
+            'lekcja' => 'text',
+            'przedmiot' => 'text',
+            'nauczyciel' => 'text',
+            'sala' => 'text',
+        ));
+
+        print <<< START
+Tworzenie tabeli: lek_godziny
+
+Zakonczono tworzenie bazy danych!
+
+START;
+
+        $isf->DbTblCreate('lek_godziny', array(
+            'lekcja' => 'text',
+            'godzina' => 'text',
+            'dl_prz' => 'text'
+        ));
+
+        print <<< START
+Wypelnianie rejestru...
+
+START;
+
+        $isf->DbInsert('rejestr', array(
+            'opcja' => 'edycja_danych',
+            'wartosc' => '1'
+        ));
+
+        $isf->DbInsert('rejestr', array(
+            'opcja' => 'ilosc_godzin_lek',
+            'wartosc' => '1'
+        ));
+
+        $isf->DbInsert('rejestr', array(
+            'opcja' => 'dlugosc_lekcji',
+            'wartosc' => '45'
+        ));
+
+        $isf->DbInsert('rejestr', array(
+            'opcja' => 'nazwa_szkoly',
+            'wartosc' => $szkola
+        ));
+
+        $isf->DbInsert('rejestr', array(
+            'opcja' => 'index_text',
+            'wartosc' => '<h1>Witaj w Planie Lekcji</h1><p>Na początek proszę zmienić hasła do panelu administracyjnego
+                oraz zmienić treść tej strony w górnym panelu użytkownika.</p><p>Dziękuję za skorzystanie z systemu Plan Lekcji</p>'
+                ), false);
+
+        $isf->DbInsert('rejestr', array(
+            'opcja' => 'ilosc_grup',
+            'wartosc' => '0'
+        ));
+
+        $isf->DbInsert('rejestr', array(
+            'opcja' => 'installed',
+            'wartosc' => '1'
+        ));
+
+        $isf->DbInsert('rejestr', array(
+            'opcja' => 'app_ver',
+            'wartosc' => '1.5 testing'
+        ));
+
+        $pass = substr(md5(@date('Y:m:d')), 0, 8);
+
+        print <<< START
+Utworzenie administratora...
+    
+START;
+
+        $isf->DbInsert('uzytkownicy', array(
+            'login' => 'administrator',
+            'haslo' => md5($pass = (rand(1, 100) . $pass))
+        ));
+
+        print <<< START
+
+INSTALACJA ZAKONCZONA POWODZENIEM!
+        
+* Prosze zapisac dane oraz usunac plik install.php,
+    aby kontynuowac prace z systemem
+        
+Prosze zapisac ponizsze dane, aby uzyskac dostep do panelu administratora
+
+    Login: <b>administrator</b>
+    Haslo: <b>$pass</b>
+
+Gdy plik install.php zostanie usunięty przejdz do <a href="index.php">strony glownej</a>
+START;
+        if ($ferr == true) {
+            echo '<br/><b>BŁĄD ZAPISU: config.php</b><br/>
+                Proszę utworzyć plik <b>config.php</b> z następującą treścią:<br/><br/>
+                <?php $path = \'' . $_POST['inpPath'] . '\'; ?>';
+        }
+        echo '</pre>';
+        ?>
+    <?php endif; ?>
+<?php endif; ?>
