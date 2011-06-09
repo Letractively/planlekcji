9398839f9ca9fff161a6e98017ac83bf5735bfdd
mhl.bocian@gmail.com <mhl.bocian@gmail.com@7e1661bb-de51-e99c-37c1-f5beeeb525d3>
2011-06-09 13:49:16 +0000

    Poprawki w szablonach

--- branches/planlekcji-1.0dev/install.php
+++ branches/planlekcji-1.0dev/install.php
@@ -80,7 +80,7 @@ if (count($ctb) != 0) {
 + + + + + + + + + + + + + + + + + + + +
 +                                     +
 +    I   N   T   E    R   S   Y   S   +  Wersja 1.0
-+    P  L  A  N   L  E  K  C  J  I    + 
++    P  L  A  N   L  E  K  C  J  I    +  DEVELOPER
 +                                     +
 + + + + + + + + + + + + + + + + + + + +
 
@@ -269,7 +269,7 @@ START;
         
         $isf->DbInsert('rejestr', array(
             'opcja' => 'app_ver',
-            'wartosc' => '1.0.1'
+            'wartosc' => '1.0 dev'
         ));
 
         $pass = substr(md5(@date('Y:m:d')), 0, 8);
